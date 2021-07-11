955bba39aba64f58cbec64144a2ce1a1fadddd1c
html2021-1 <58695492+html2021-1@daum.net>
2021-07-11 15:16:32 +0900

    pc fullpage 이동시 .moveup 애니메이트

--- js/main.js
+++ js/main.js
@@ -34,25 +34,11 @@ $(document).ready(function () {
     $(this).addClass('hidden').siblings().removeClass('hidden');
   });
 
-  // pc에서 .bottom_btn 클릭 시 #cnt2로 이동
-  $("#pc_bottom_btn").on("click", function() {
-    fullpage_api.moveTo(2);
+  // .btm_btn 클릭 시 #cnt2로 이동
+  $(".btm_btn").on("click", function() {
+    if ($(window).width() > 1152) fullpage_api.moveTo(2);  // pc
+    else $('html, body').stop().animate({scrollTop: $('#cnt2').offset().top}, 600);  // 태블릿, 모바일
   })
-
-  // 태블릿, 모바일에서 .bottom_btn 클릭 시 #cnt2로 이동
-  $("#m_bottom_btn").on("click", function() {
-    $('html, body').stop().animate({
-        scrollTop: $('#cnt2').offset().top
-    }, 600);
-  })
-
-  // #cnt2 ~ #cnt6이 .active를 가질 때 애니메이션 클래스 추가
-  // $(window).on('scroll', function () {
-  //   const $cntAni = $('#fullpage .ani');
-  //   if ($cntAni.is('.active')) $cntAni.addClass('movetop');
-  //   else $cntAni.removeClass('movetop');
-  // });
-
 
   // #cnt3 #book_list li a에 마우스와 포커스가 진입하거나 빠져 나갈 때
   const $listEle = $('#book_list li a');
